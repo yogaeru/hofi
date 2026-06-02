6e8aed283330afad80560b09c917959f5921cc2b
yogaeru <yogaadhip3@gmail.com>
2026-06-02 22:53:35 +0700

    Add validate command

--- index.ts
+++ index.ts
@@ -4,40 +4,55 @@ import { logger } from "#/utils/logger";
 
 import { scanCommand } from "#/commands/scan";
 import { initCommnad } from "#/commands/init";
+import { validateCommand } from "#/commands/validate";
+import { switchCommand } from "#/commands/switch";
 
-import { getPacmanPackages } from "#/backend/pacman";
+import {
+  getPacmanPackages,
+  dryRunPacman,
+  installPacmanPackages,
+} from "#/backend/pacman";
+
 import { getCurrentDirectory } from "#/utils/path";
 
-const hofiCli = cac("hofi")
-  .version("0.1");
+const hofiCli = cac("hofi").version("0.1");
 
-hofiCli
-  .command("doctor", "Detect system environment")
-  .action(async () => {
-    await scanCommand();
-    const packages = await getPacmanPackages();
-    logger.info("Pacman packages:");
-    console.log(packages.join("\n"));
-  });
-
+hofiCli.command("scan", "Detect system environment").action(async () => {
+  await scanCommand();
+  const packages = await getPacmanPackages();
+  logger.info("Pacman packages:");
+  console.log(packages.join("\n"));
+});
 
 //-------------------- Init ----------------------
+//
 hofiCli
   .command("init", "Initialize a new hofi project with a template")
   .action(async () => {
     await initCommnad();
   });
 
+hofiCli
+  .command("validate", "Validate the hofi project configuration")
+  .action(async () => {
+    await validateCommand();
+  });
+
 // ---------------- SWITCH ----------------------
+//
 hofiCli
   .command("switch [dir]", "Switch to a different hofi project")
   .option("--dry-run", "Run the switch command without making any changes")
-  .action(async (dir?: string, options?: {dryRun?: boolean}) => {
+  .action(async (dir?: string, options?: { dryRun?: boolean }) => {
+    if (!dir) {
+      logger.error("No directory specified");
+      process.exit(1);
+    }
     const cwd = await getCurrentDirectory();
-    logger.info(`Current directory: ${cwd}`);
-    console.log(dir, options)
+    // logger.info(`Current directory: ${cwd}`);
+    // console.log(dir, options)
+    await switchCommand(dir, options);
   });
-
 
 hofiCli.help();
 
---
+++ src/commands/validate.ts
@@ -0,0 +1,25 @@
+import { parseConfigTOML } from "#/utils/parser";
+import { ConfigSchema, type Config } from "#/lib/schema";
+
+export async function validateCommand() {
+  try {
+    const configTOML = await parseConfigTOML("hofi/generated/config.toml");
+    const result: Config = ConfigSchema.parse(configTOML);
+
+    // console.log("[INFO] config: ", configTOML);
+    // console.log("[INFO] result: ", result);
+    // 
+    for (const [key, value] of Object.entries(result)) {
+      console.log(`[INFO] ${key}`);
+      if (typeof value === "object") {
+        for (const [subKey, subValue] of Object.entries(value)) {
+          console.log(`[INFO] -- ${subKey}`);
+        }
+      }
+    }
+
+    console.log("[INFO] validation successful, no errors founds");
+  } catch (error) {
+    console.error("[ERROR] ", error);
+  }
+}
