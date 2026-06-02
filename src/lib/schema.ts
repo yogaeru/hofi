5a4d97ca474cb8a0da3f4913018a743fd9a2190a
yogaeru <yogaadhip3@gmail.com>
2026-06-02 22:54:50 +0700

    Add schema use optonal and partial

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,43 +1,60 @@
 import { z } from "zod";
 
-export const ConfigSchema = z.object({
-  user: z.object({
-    name: z.string(),
-    homeDir: z.string(),
-  }),
+export const ConfigSchema = z
+  .object({
+    user: z
+      .object({
+        name: z.string(),
+        homeDir: z.string(),
+      })
+      .partial(),
 
-  packages: z.object({
-    pacman: z.array(z.string()),
-    aur: z.array(z.string()),
-    flatpak: z.array(z.string()),
-  }),
+    packages: z
+      .object({
+        pacman: z.array(z.string()),
+        aur: z.array(z.string()),
+        flatpak: z.array(z.string()),
+      })
+      .partial()
+      .optional(),
 
-  default: z.object({
-    apps: z.object({
-      browser: z.string(),
+    default: z
+      .object({
+        apps: z
+          .object({
+            browser: z.string(),
+            editor: z.string(),
+          })
+          .partial(),
+
+        mime: z
+          .object({
+            defaultApplications: z.record(z.string(), z.string()),
+            addedAssociations: z.record(z.string(), z.string()),
+          })
+          .partial(),
+      })
+      .partial(),
+
+    mkSymlink: z.record(
+      z.string(), // source path
+      z.string(), // target path
+    ),
+
+    diskMount: z.record(
+      z.string(), // mount point (mis. "/mnt/data")
+      z.object({
+        description: z.string(),
+        uuid: z.string(),
+        type: z.string(),
+        options: z.array(z.string()),
+        wantedBy: z.string(),
+      }),
+    ),
+    sources: z.object({
+      include: z.array(z.string()),
     }),
-
-    mime: z.object({
-      defaultApplication: z.record(z.string(), z.string()),
-      addedAssociations: z.record(z.string(), z.string()),
-    }),
-  }),
-
-  mkSymlink: z.record(
-    z.string(), // source path
-    z.string(), // target path
-  ),
-
-  diskMount: z.record(
-    z.string(), // mount point (mis. "/mnt/data")
-    z.object({
-      description: z.string(),
-      uuid: z.string(),
-      type: z.string(),
-      options: z.array(z.string()),
-      wantedBy: z.string(),
-    }),
-  ),
-});
+  })
+  .partial();
 
 export type Config = z.infer<typeof ConfigSchema>;
