ff145c6b3b7e248aa1650e1c2605635cf72934e8
yogaeru <yogaadhip3@gmail.com>
2026-06-06 08:37:48 +0700

    Update return validated config

--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -1,14 +1,14 @@
 import { parseConfigTOML } from "#/utils/parser";
 import { ConfigSchema, type Config } from "#/lib/schema";
 
-export async function validateCommand() {
+export async function validateCommand(): Promise<Config | undefined> {
   try {
     const configTOML = await parseConfigTOML("hofi/generated/config.toml");
     const result: Config = ConfigSchema.parse(configTOML);
 
-    // console.log("[INFO] config: ", configTOML);
-    // console.log("[INFO] result: ", result);
-    // 
+    console.log("[INFO] config: ", configTOML);
+    console.log("[INFO] result: ", result);
+    //
     for (const [key, value] of Object.entries(result)) {
       console.log(`[INFO] ${key}`);
       if (typeof value === "object") {
@@ -19,6 +19,7 @@ export async function validateCommand() {
     }
 
     console.log("[INFO] validation successful, no errors founds");
+    return result;
   } catch (error) {
     console.error("[ERROR] ", error);
   }
