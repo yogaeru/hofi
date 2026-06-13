e35c51dc6e5e14b74ef239f46196b675bb4c457f
yogaeru <yogaadhip3@gmail.com>
2026-06-13 11:56:27 +0700

    Refactor and fix order cheking

--- src/core/symlink.ts
+++ src/core/symlink.ts
@@ -11,46 +11,58 @@ export async function mkSymlinkConfig(config: Record<string, string>) {
   await Promise.all(
     Object.entries(config).map(
       async ([target, symlink]: [string, string]): Promise<void> => {
-        const [resolvedTarget, resolvedSymlink] = await Promise.all([
-          resolvePath(target),
-          resolvePath(symlink),
-        ]);
-
-        const [isBroken, isSymlinkExist, isTargetExist, isPathExist] =
-          await Promise.all([
-            isBrokenSymlink(resolvedSymlink),
-            isSymlink(resolvedSymlink),
-            exists(resolvedTarget),
-            exists(resolvedSymlink),
-          ]);
-
-        if (isPathExist) {
-          if (isSymlinkExist) return;
-          const archive = await createBackupConfig(resolvedSymlink);
-          if (archive) logger.info(`BACKUP: ${resolvedSymlink} -> ${archive}`);
-        }
-
-        if (!isTargetExist) {
-          throw new Error(`Target path does not exist: ${resolvedTarget}`);
-        }
-
-        if (isSymlinkExist) {
-          if (isBroken) await recreateSymlink(resolvedTarget, resolvedSymlink);
-
-          return;
-        }
-
-        await createSymlink(resolvedTarget, resolvedSymlink);
+        await syncSymlink(target, symlink);
       },
     ),
   );
 }
 
 /**
+ * Synchronizes a symlink from `target` to `symlink`, creating or updating it as necessary.
+ * @param target The target path to link to.
+ * @param symlink The symlink path to create or update.
+ * @returns A promise that resolves when the symlink has been synchronized.
+ */
+async function syncSymlink(target: string, symlink: string) {
+  const [resolvedTarget, resolvedSymlink] = await Promise.all([
+    resolvePath(target),
+    resolvePath(symlink),
+  ]);
+
+  const [brokenSymlink, symlinkExist, isTargetPathExist, isSymlinkPathExist] =
+    await Promise.all([
+      isBrokenSymlink(resolvedSymlink),
+      isSymlink(resolvedSymlink),
+      exists(resolvedTarget),
+      exists(resolvedSymlink),
+    ]);
+
+  if (!isTargetPathExist) {
+    throw new Error(`Target path does not exist: ${resolvedTarget}`);
+  }
+
+  if (symlinkExist) return;
+
+  if (brokenSymlink) {
+    await recreateSymlink(target, symlink);
+    logger.info(`RECREATE SYMLINK: ${resolvedTarget} -> ${resolvedSymlink}`);
+    return;
+  }
+
+  if (isSymlinkPathExist) {
+    const archive: string = await createBackupConfig(resolvedSymlink);
+    if (archive) logger.info(`BACKUP: ${resolvedSymlink} -> ${archive}`);
+  }
+
+  await createSymlink(resolvedTarget, resolvedSymlink);
+  logger.info(`SYMLINK: ${resolvedTarget} -> ${resolvedSymlink}`);
+}
+
+/**
  *  Create a symlink from `target` to `symlink`.
  * @param target
  * @param symlink
- * @returns
+ * @returns Boolean indicating whether the symlink was created successfully.
  */
 export async function createSymlink(target: string, symlink: string) {
   try {
@@ -61,6 +73,12 @@ export async function createSymlink(target: string, symlink: string) {
   }
 }
 
+/**
+ * Recreate a symlink from `target` to `symlink`.
+ * @param target
+ * @param symlink
+ * @returns Boolean indicating whether the symlink was recreated successfully.
+ */
 export async function recreateSymlink(target: string, symlink: string) {
   try {
     await $`ln -sfn ${target} ${symlink}`;
@@ -70,7 +88,12 @@ export async function recreateSymlink(target: string, symlink: string) {
   }
 }
 
-export async function isBrokenSymlink(symlink: string): Promise<boolean> {
+/**
+ * Check if a symlink is broken.
+ * @param symlink
+ * @returns Boolean indicating whether the symlink is broken.
+ */
+async function isBrokenSymlink(symlink: string): Promise<boolean> {
   const resolvedSymlink = await resolvePath(symlink);
 
   try {
@@ -81,7 +104,12 @@ export async function isBrokenSymlink(symlink: string): Promise<boolean> {
   }
 }
 
-export async function isSymlink(symlink: string): Promise<boolean> {
+/**
+ * Check if a symlink is a symlink.
+ * @param symlink
+ * @returns Boolean indicating whether the symlink is a symlink.
+ */
+async function isSymlink(symlink: string): Promise<boolean> {
   const resolvedSymlink = await resolvePath(symlink);
 
   try {
