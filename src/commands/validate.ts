2d0996f2ef23ee74aed2cf8fcd5f86a48c9ee646
yogaeru <yogaadhip3@gmail.com>
2026-06-17 20:58:50 +0700

    Update support validate from given path

--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -1,11 +1,12 @@
 import { logger } from "#/utils/logger";
 import { parseConfigTOML } from "#/utils/parser";
-import { ConfigSchema, type Config } from "#/lib/schema";
+import { resolvePath, getConfigPath } from "#/utils/path";
 import {
   validateSymlink,
   validateMountDisk,
   validatePackages,
 } from "#/utils/validation";
+import { ConfigSchema, type Config } from "#/lib/schema";
 
 /**
  * Validates the configuration file at the given path, or the default path if none is provided.
@@ -17,9 +18,17 @@ export async function validateCommand(
   path?: string,
 ): Promise<Config | undefined> {
   try {
-    const configPath = path ?? "hofi/generated/config.toml";
-    const configTOML = await parseConfigTOML(configPath);
+    const resolvedPath: string = path ? await resolvePath(path) : process.cwd();
+    const configPath: string = await getConfigPath(resolvedPath);
 
+    if (!path) {
+      logger.warn(
+        `No path provided, using current working directory: ${resolvedPath}`,
+      );
+    }
+    logger.info(`Validating configuration file (${configPath})`);
+    logger.info("Parsing configuration file..." + configPath);
+    const configTOML = await parseConfigTOML(configPath);
     const result: Config = ConfigSchema.parse(configTOML);
 
     const { mkSymlink, diskMount, packages } = result;
@@ -30,10 +39,9 @@ export async function validateCommand(
       validatePackages(packages?.pacman, "pacman"),
     ]);
 
-    logger.info("Validation successful, no errors founds");
+    logger.success("Validation successful, no errors found");
     return result;
   } catch (error) {
-    logger.error(String(error));
-    // console.log(error)
+    logger.error(error);
   }
 }
