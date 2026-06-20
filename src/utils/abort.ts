0bac9d71bb4bea10a00e5c15fe822c53002e7d6f
yogaeru <yogaadhip3@gmail.com>
2026-06-20 20:44:37 +0700

    Add helper to abort process

---
+++ src/utils/abort.ts
@@ -0,0 +1,21 @@
+import { isRunWithSudo } from "#/core/detect";
+import { logger } from "#/utils/logger";
+
+export function abortIfRunWithSudo() {
+  if (isRunWithSudo()) {
+    abort("Don't run as root. Please run the command as a regular user.");
+  }
+}
+
+export function abort(message: string): never {
+  logger.error(message);
+  process.exit(1);
+}
+
+export function tryAbort(message: string, callbackFn: () => any) {
+  try {
+    return callbackFn();
+  } catch (error) {
+    abort(message);
+  }
+}
